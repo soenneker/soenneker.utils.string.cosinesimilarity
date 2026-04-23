08b30c5dfe1b843b0fbf96ffdb550cb7b5d70d35
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:04 -0500

    Various fixes

--- test/Soenneker.Utils.String.CosineSimilarity.Tests/CosineSimilarityStringUtilTests.cs
+++ test/Soenneker.Utils.String.CosineSimilarity.Tests/CosineSimilarityStringUtilTests.cs
@@ -12,18 +12,18 @@ public class CosineSimilarityStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("", "", 1)]
-    [InlineData("abc", "", 0)]
-    [InlineData("", "xyz", 0)]
-    [InlineData("kitten", "sitting", 0)]
-    [InlineData("kitten", "kitten", 1)]
-    [InlineData("abc", "def", 0)]
-    [InlineData("abcdef", "abc", 0)]
-    [InlineData("abc", "abcd", 0)]
-    [InlineData("this is sitting on a porch", "this is sitting", .7071)]
-    [InlineData("the cat sat on a hat", "sad on a hat", .6123)]
-    [InlineData("this is a test", "this is another test", .75)]
+    [Test]
+    [Arguments("", "", 1)]
+    [Arguments("abc", "", 0)]
+    [Arguments("", "xyz", 0)]
+    [Arguments("kitten", "sitting", 0)]
+    [Arguments("kitten", "kitten", 1)]
+    [Arguments("abc", "def", 0)]
+    [Arguments("abcdef", "abc", 0)]
+    [Arguments("abc", "abcd", 0)]
+    [Arguments("this is sitting on a porch", "this is sitting", .7071)]
+    [Arguments("the cat sat on a hat", "sad on a hat", .6123)]
+    [Arguments("this is a test", "this is another test", .75)]
     public void CalculateSimilarity_Returns_Correct_Similarity_Score(string str1, string str2, double expectedScore)
     {
         double similarityScore = CosineSimilarityStringUtil.CalculateSimilarity(str1, str2);
@@ -32,15 +32,15 @@ public class CosineSimilarityStringUtilTests : HostedUnitTest
     }
 
 
-    [Theory]
-    [InlineData("", "", 100.0)]
-    [InlineData("abc", "", 0.0)]
-    [InlineData("", "xyz", 0.0)]
-    [InlineData("kitten", "sitting", 0)]
-    [InlineData("kitten", "kitten", 100.0)]
-    [InlineData("abc", "def", 0.0)]
-    [InlineData("abcdef", "abc", 0.0)]
-    [InlineData("this is sitting on a porch", "this is sitting", 70.71)]
+    [Test]
+    [Arguments("", "", 100.0)]
+    [Arguments("abc", "", 0.0)]
+    [Arguments("", "xyz", 0.0)]
+    [Arguments("kitten", "sitting", 0)]
+    [Arguments("kitten", "kitten", 100.0)]
+    [Arguments("abc", "def", 0.0)]
+    [Arguments("abcdef", "abc", 0.0)]
+    [Arguments("this is sitting on a porch", "this is sitting", 70.71)]
     public void CalculateSimilarityPercentage_Returns_Correct_Percentage(string str1, string str2, double expectedPercentage)
     {
         double similarityPercentage = CosineSimilarityStringUtil.CalculateSimilarityPercentage(str1, str2);
